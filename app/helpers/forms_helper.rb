0631c18c048487fb0491fc8e52c61d0338c9c1fc
Todd Gehman <toddgehman@gmail.com>
2016-03-02 11:27:57 -0500

    fancier styling for public stuff

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -14,6 +14,26 @@ module FormsHelper
     content_tag(:div, contents, options.merge(class: 'row'))
   end
 
+  def icon_field(f, field_name, icon_name = field_name)
+    icon = self.send("#{icon_name}_icon")
+    icon_html = content_tag(:span, icon, class: 'field-icon')
+
+    field_type = :text
+    [:email, :password, :submit].each do |token|
+      if field_name.to_sym == token
+        field_type = field_name
+      end
+    end
+
+    field_method = "#{field_type}_field"
+    if field_type.to_sym == :submit
+      field_html = submit_button(f, field_name.to_s.titleize, 'Sending...', class: 'field-input')
+    else
+      field_html = f.send(field_method, field_name, placeholder: field_name.to_s.titleize, class: 'field-input')
+    end
+    return content_tag(:div, icon_html + field_html, class: ['icon-field', field_type])
+  end
+
   def form_label(label)
     content_tag(:div, label, class: 'small-12 medium-3 large-3 columns form-label')
   end
@@ -22,8 +42,13 @@ module FormsHelper
     content_tag(:div, field, class: 'small-12 medium-9 large-9 columns form-field')
   end
 
-  def submit(f, text = 'Save', disable_text = 'Saving...')
-    label_field(nil, f.submit(text, data: {disable_with: disable_text}))
+  def submit(f, text = 'Save', disable_text = 'Saving...', options = {})
+    label_field(nil, submit_button(f, text, disable_text, options))
+  end
+
+  def submit_button(f, text = 'Save', disable_text = 'Saving...', options = {})
+    f.submit(text, options.merge(data: {disable_with: disable_text}))
   end
 
 end
+
