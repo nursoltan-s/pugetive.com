7f2f1ea07fe5069fdc8d6fc54ac4a9a4baa20882
Todd Gehman <toddgehman@gmail.com>
2016-03-17 15:12:15 -0400

    remember location, canonize domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,33 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :remember_location, :canonize_domain
+
   after_action :verify_authorized, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
+
+  private
+
+    def remember_location
+      if ['show', 'index'].include?(params[:action])
+        set_return_to
+      end
+    end
+
+    def set_return_to
+      session[:return_to] = request.fullpath
+    end
+
+    def canonize_domain
+      return unless request.subdomains.any?
+      if request.host_with_port != CONFIG[Rails.env][:host_with_port]
+        permanent_redirect("#{CONFIG[Rails.env][:host_protocol]}://#{CONFIG[Rails.env][:host_with_port]}#{request.path}")
+      end
+    end
+
+    def permanent_redirect(url_options)
+      flash.keep
+      redirect_to(url_options, :status => 301)
+    end
 end
