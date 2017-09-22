4180a39c3ce3bd7ac3fabd50e7cec9583f8d560e
Todd Gehman <toddgehman@gmail.com>
2017-09-22 13:42:50 -0400

    link photos to their source page

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,11 +4,21 @@ module LinksHelper
     "/touch/#{subject.class.name.downcase}/#{subject.id}"
   end
 
-  def flickr_link(flickr_id)
-    link_to(raw("Flickr #{flickr_icon}"), "https://www.flickr.com/photos/pugetive/#{flickr_id}/")
+  def photo_link(work, content = "Photo")
+    if work.flickr_id.present?
+      return flickr_link(work.flickr_id, content)
+    elsif work.instagram_id.present?
+      return instagram_link(work.instagram_id, content)
+    end
   end
 
-  def instagram_link(instagram_id)
-    link_to(raw("Instagram #{instagram_icon}"), "https://www.instagram.com/p/#{instagram_id}")
+  def flickr_link(flickr_id, content = "Flickr #{flickr_icon}")
+    link_to(raw(content), "https://www.flickr.com/photos/pugetive/#{flickr_id}/", target: "_blank")
   end
+
+  def instagram_link(instagram_id, content = "Instagram #{instagram_icon}")
+    link_to(raw(content), "https://www.instagram.com/p/#{instagram_id}", target: "_blank")
+  end
+
+
 end
