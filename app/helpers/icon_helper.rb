d32970a6975b08396c9be17b68579a1d75950ec8
Todd Gehman <toddgehman@gmail.com>
2017-11-26 20:49:44 -0500

    adding mobile icons

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -197,6 +197,11 @@ module IconHelper
     end
   end
 
+  def mobile_icons
+    raw(apple_icons + windows_icons + android_icons)
+  end
+
+
   private
 
     def awesome_icon(token, options = {})
@@ -205,6 +210,61 @@ module IconHelper
 
     def foundation_icon(token, options = {})
       content_tag(:i, nil, class: ['icon', "fi-#{token}"])
+    end
+
+    def apple_icons
+      # <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+      # <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+      # ...etc
+      links = [favicon_link_tag("icons/favicons/apple-touch-icon.png",
+                                rel: 'apple-touch-icon',
+                                type: 'image/png')]
+
+       # [[16, 16],
+       #  [32, 32],
+       #  [57, 57],
+       #  [60, 60],
+       #  [72, 72],
+       #  [76, 76],
+       #  [96, 96],
+       #  [114, 114],
+       #  [120, 120],
+       #  [144, 144],
+       #  [152, 152],
+       #  [180, 180]].each do |size_pair|
+
+       [[16, 16],
+        [32, 32],
+        [96, 96]].each do |size_pair|
+          real_dimension = size_pair.first
+          size_attr = size_pair.last || real_dimension
+          links << favicon_link_tag("icons/favicons/apple-touch-icon-#{real_dimension}x#{real_dimension}.png",
+                                    rel:  'apple-touch-icon',
+                                    type: 'image/png',
+                                    sizes: "#{size_attr}x#{size_attr}")
+
+      end
+
+      links.join("\n")
+    end
+
+    def android_icons
+      favicon_link_tag("icons/favicons/android-chrome-192x192.png",
+                       rel: "icon",
+                       type: "image/png",
+                       href: "/",
+                       sizes: "192x192")
+
+
+    end
+
+    def windows_icons
+      links = []
+      links << content_tag(:meta, nil, name: "msapplication-square70x70logo", content: asset_path("icons/favicons/smalltile.png"))
+      links << content_tag(:meta, nil, name: "msapplication-square150x150logo", content: asset_path("icons/favicons/mediumtile.png"))
+      links << content_tag(:meta, nil, name: "msapplication-wide310x150logo", content: asset_path("icons/favicons/widetile.png"))
+      links << content_tag(:meta, nil, name: "msapplication-square310x310logo", content: asset_path("icons/favicons/largetile.png"))
+      links.join("\n")
     end
 
 
