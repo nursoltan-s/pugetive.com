28e0b801c82f8938348c254336d5ac4415284f4b
Todd Gehman <toddgehman@gmail.com>
2017-12-02 22:50:10 -0500

    fixed doubled title on works pages, etc

--- config/initializers/globals.rb
+++ config/initializers/globals.rb
@@ -1,7 +1,8 @@
-# SITE_TITLE = "Todd G @ pugetive.com"
 BOOLEAN_OPTIONS = [0, 1, false, true]
+
 DIVIDER = '~'
 SITE_DESCRIPTION = 'Online hub for Todd Gehman (internet handle: pugetive) and his various amateur and professional projects.'
+
 US_STATES = Carmen::Country.coded('US').subregions.map{|subregion| [subregion.code, subregion.name]}
 YEARS_OF_LIFE = Time.now.year.downto(1970)
 
