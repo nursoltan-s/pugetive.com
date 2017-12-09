e3e92272ef69d385dd5982ddfba773e82ea043a7
Todd Gehman <toddgehman@gmail.com>
2017-12-08 19:57:59 -0500

    return db to linked config files after reverting attempts to switch to ENV

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,8 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_files, %w{config/honeybadger.yml
+set :linked_files, %w{config/database.yml
+                      config/honeybadger.yml
                       config/pugetive.yml
                       config/secrets.yml
                       config/newrelic.yml
