e978a6d6bad079ba4fbb0ac1ebba714990cdf969
Todd Gehman <toddgehman@gmail.com>
2017-12-09 19:10:52 -0500

    removed comment cruft

--- app/models/works/model_image.rb
+++ app/models/works/model_image.rb
@@ -6,12 +6,6 @@ class ModelImage < HostedImage
   end
 
   def thumbnail(linked = true, options = {})
-   # <div class="screenshot">
-   #   <a target="_blank" href="https://cdn.toddgehman.com/work/115/work-115.115.large.png?1511714957">
-   #     <img class="thumbnail" src="https://cdn.toddgehman.com/work/115/work-115.115.thumb.png?1511714957" alt="Work 115.115.thumb">
-   #    </a>
-   #  </div>
-
     if linked
       return link_to(img_tag(options.merge(class: 'thumbnail')), work.image.url(:large), {target: "_blank"})
     end
@@ -30,18 +24,3 @@ class ModelImage < HostedImage
     end
 
 end
-
-# def thumbnail(link_to_original = false)
-#   return nil unless model.has_image?
-#   rv = ''
-#   image_html = h.image_tag(model.image.url(:thumb), class: 'thumbnail')
-#   if link_to_original
-#     contents = h.link_to(image_html, model.image.url(:large), target: "_blank")
-#   elsif model.url.present? and model.live?
-#     contents = h.link_to(image_html, model.url)
-#   else
-#     contents = image_html
-#   end
-#   h.content_tag(:div, contents, class: 'screenshot')
-# end
-
