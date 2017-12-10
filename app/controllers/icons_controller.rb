76a76636e1dbe18aca5943688dd8eec7dbadc0df
Todd Gehman <toddgehman@gmail.com>
2017-12-10 14:10:55 -0500

    redirect back to icons index after update

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -4,6 +4,10 @@ class IconsController < MetaResourceController
     icons_path
   end
 
+  def post_update_path
+    icons_path
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     # Never trust parameters from the scary internet, only allow the white list through.
