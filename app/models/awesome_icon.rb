cdd271e727002f044c9acbacf6c2ce6fbb32b4b4
Todd Gehman <toddgehman@gmail.com>
2017-12-10 13:48:26 -0500

    allow icons to be editied but block renaming of fallback icon

--- app/models/awesome_icon.rb
+++ app/models/awesome_icon.rb
@@ -1,5 +1,9 @@
 class AwesomeIcon < Icon
 
+  def self.model_name
+    Icon.model_name
+  end
+
   def to_html
     ActionController::Base.helpers.content_tag(:i, nil, class: ['fa', "fa-#{token}", 'icon'])
   end
--- app/models/foundation_icon.rb
+++ app/models/foundation_icon.rb
@@ -1,5 +1,9 @@
 class FoundationIcon < Icon
 
+  def self.model_name
+    Icon.model_name
+  end
+
   def to_html
     ActionController::Base.helpers.content_tag(:i, nil, class: ["fi-#{token}", 'icon'])
   end
