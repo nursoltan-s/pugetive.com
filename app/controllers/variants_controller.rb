fee5f4fbbd41a3adfe32f23368fb6cf445517598
Todd Gehman <toddgehman@gmail.com>
2017-12-26 13:40:31 -0500

    basic layout for variants

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -8,10 +8,17 @@ class VariantsController < MetaResourceController
 
   private
     def variant_params
-      params.require(:variant).permit(:work_id, :name, :year)
+      params.require(:variant).permit(:name,
+                                      :work_id,
+                                      :year,
+                                      documents_attributes: [:data, :name])
     end
 
     def post_create_path
+      @variant.work
+    end
+
+    def post_update_path
       @variant.work
     end
 
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,4 +1,7 @@
 class Variant < ApplicationRecord
+
+  include Documentable
+
   validates :work_id, presence: true
   validates :name,    presence: true
   validates :year, inclusion: {in: YEARS_OF_LIFE}, allow_blank: true
