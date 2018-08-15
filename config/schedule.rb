b3983aa77a0e3bdf3d4d22628fee0c404d01e551
Todd Gehman <t.gehman@celmatix.com>
2018-08-15 12:13:46 -0400

    whenever cron for africa reminders

--- config/schedule.rb
+++ config/schedule.rb
@@ -39,3 +39,7 @@ end
 every 1.day, :at => '5:00 am' do
   rake "-s sitemap:refresh"
 end
+
+every 1.day, at: ['9:00 am'] do
+  command "cd /home/deployer/apps/pugetive/current;ruby ./lib/africa/africa_reminder_mail.rb"
+end
