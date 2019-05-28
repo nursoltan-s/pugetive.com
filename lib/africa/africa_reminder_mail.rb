b6dafec13ec05e6b07dfbcee617854f6767ba1ce
Todd Gehman <t.gehman@celmatix.com>
2019-05-28 09:33:36 -0400

    new trailmix offset indexed for novel writing

--- lib/africa/africa_reminder_mail.rb
+++ lib/africa/africa_reminder_mail.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'mail'
 
-OFFSET_PAGES = 710
+OFFSET_PAGES = 833
 
 gpass = YAML.load_file("config/pugetive.yml")['production']['gmail_app_key']
 options = { :address              => "smtp.gmail.com",
